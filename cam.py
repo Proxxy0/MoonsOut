97634a1c7ddd6f47a5d4176dd989490f059d478e
Proxxy0 <noah.worley921@gmail.com>
2020-12-18 22:08:25 -0500

    changed some variables and added normalized images

--- cam.py
+++ cam.py
@@ -5,7 +5,7 @@ import numpy as np
 camera = cv2.VideoCapture(0)
 images = []
 tic = datetime.datetime.now()
-for i in range(20):
+for i in range(10):
 	return_value, image = camera.read()
 	images.append(image)
 	#cv2.imwrite('opencv'+str(i)+'.png', image)
@@ -28,8 +28,8 @@ cv2.imwrite('averaged.png', dst)
 
 greyImage = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
 
-alpha=1.5
-beta=20
+alpha=3
+beta=100
 
 new_image=cv2.addWeighted(dst,alpha,np.zeros(dst.shape, dst.dtype),0,beta)
 new_image2=cv2.addWeighted(greyImage,alpha,np.zeros(greyImage.shape, greyImage.dtype),0,beta)
@@ -37,3 +37,9 @@ new_image2=cv2.addWeighted(greyImage,alpha,np.zeros(greyImage.shape, greyImage.d
 cv2.imwrite('greyscale.png',greyImage)
 cv2.imwrite('contboost.png', new_image)
 cv2.imwrite('contboost2.png', new_image2)
+
+normalized = cv2.normalize(dst, None, 0, 255, cv2.NORM_MINMAX)
+normalized2 = cv2.normalize(new_image, None, 0, 255, cv2.NORM_MINMAX)
+
+cv2.imwrite('normalized.png',normalized)
+cv2.imwrite('normalized2.png', normalized2)
