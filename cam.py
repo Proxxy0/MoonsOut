ed821f3bd28497bf14ae1fb7d2df9c821d7074a9
Proxxy0 <noah.worley921@gmail.com>
2020-12-18 21:41:02 -0500

    added stacking test code

---
+++ cam.py
@@ -0,0 +1,39 @@
+import cv2
+import datetime
+import numpy as np
+
+camera = cv2.VideoCapture(0)
+images = []
+tic = datetime.datetime.now()
+for i in range(20):
+	return_value, image = camera.read()
+	images.append(image)
+	#cv2.imwrite('opencv'+str(i)+'.png', image)
+del(camera)
+toc = datetime.datetime.now()
+
+dst = images[0]
+for i in range(len(images)):
+	if i == 0:
+		pass
+	else:
+		alpha = 1.0/(i + 1)
+		beta = 1.0 - alpha
+		dst = cv2.addWeighted(images[i], alpha, dst, beta, 0.0)
+ 
+# Save blended image
+print(str(toc-tic))
+cv2.imwrite('firstimg.png',images[0])
+cv2.imwrite('averaged.png', dst)
+
+greyImage = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
+
+alpha=1.5
+beta=20
+
+new_image=cv2.addWeighted(dst,alpha,np.zeros(dst.shape, dst.dtype),0,beta)
+new_image2=cv2.addWeighted(greyImage,alpha,np.zeros(greyImage.shape, greyImage.dtype),0,beta)
+
+cv2.imwrite('greyscale.png',greyImage)
+cv2.imwrite('contboost.png', new_image)
+cv2.imwrite('contboost2.png', new_image2)
