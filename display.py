b3da47141b33b9fd04112170e38f4750cd852615
Proxxy0 <noah.worley921@gmail.com>
2020-12-23 19:12:33 -0500

    Webcam is now shown

--- display.py
+++ display.py
@@ -1,4 +1,5 @@
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, uic, QtCore
+from PyQt5.QtGui import QPixmap,QImage
 import sys
 import cv2
 
@@ -10,9 +11,31 @@ class Ui(QtWidgets.QDialog):
         box2 = ['Base','Red','Green','Blue','Remerged']
         self.filterBox.addItems(box1)
         self.cameraBox.addItems(box2)
+
+        self.camera = cv2.VideoCapture(0)
+        self.initUI()
+
         self.show() # Show the GUI
 
-        def setData(self, **index**, value):
+    def initUI(self):
+        self.updateSelf()
+
+        self.my_timer = QtCore.QTimer()
+        self.my_timer.timeout.connect(self.updateSelf)
+        self.my_timer.start(1) #1 min intervall
+
+
+    def updateSelf(self):
+        ret_val, img = self.camera.read()
+
+        height, width, channel = img.shape
+        bytesPerLine = 3 * width
+        qImg = QImage(img.data, width, height, bytesPerLine, QImage.Format_BGR888)
+        map = QPixmap(qImg)
+
+        self.display.setPixmap(map)
+
+        self.update()
 
 
 
